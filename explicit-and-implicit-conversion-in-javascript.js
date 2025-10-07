d99463cc6c155aeb48721ab806f08949381309c0
Sandhya Pereddy <sandhya.pereddy@gmail.com>
2025-10-06 23:23:02 -0500

    after fixing code

--- explicit-and-implicit-conversion-in-javascript.js
+++ explicit-and-implicit-conversion-in-javascript.js
@@ -19,7 +19,7 @@ Use console.log() to clearly show the before-and-after type conversions.
 */
 
 
-let result = "5" - 2;
+let result = "5" - 2;   //Changing the string to number. No need of any type conversion if we give directly number in the place of "5" 
 console.log("The result is: " + result);
 
 let isValid = Boolean("false");
@@ -28,5 +28,27 @@ if (isValid) {
 }
 
 let age = "25";
-let totalAge = age + 5;
+let totalAge = Number(age) + 5;  // If we don't do type conversion, This treat as string concatention and print 255
 console.log("Total Age: " + totalAge);
+
+
+let res = true + 1;
+console.log("Example for implicit conversion: Here true is converted to value 1 while addition")
+console.log(res);
+
+let num = 12;
+console.log("Here we are checking is the value number.")
+if(isNaN(num)) {
+  console.log("This is not a number");
+} else {
+  console.log("This is a number");
+}
+
+
+let firstItemPrice = "5";
+let secondItemPrice = 4;
+console.log("Example for explicit conversion: below we are converting to number to show that total price of two items")
+console.log("The item price is : " + firstItemPrice + secondItemPrice); // Here is the result with out type conversion
+console.log( Number(firstItemPrice) + secondItemPrice);   // Here we converting to number to get total price  
+
+
